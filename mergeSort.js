a3e790cbf1726b5ffeae1b7fc3ffcaaa891be4ef
FadilMS18 <muhfadilms2418@gmail.com>
2024-09-13 19:49:56 +0800

    Added mergeSort & Remove Duplicate function

---
+++ mergeSort.js
@@ -0,0 +1,44 @@
+function merge(arr1, arr2){
+    let sorted = []
+    let i = 0
+    let j = 0
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            sorted.push(arr1[i])
+            i++
+        }else{
+            sorted.push(arr2[j])
+            j++
+        }
+    }
+    for(; i < arr1.length; i++){
+        sorted.push(arr1[i])
+    }
+    for(; j< arr2.length; j++){
+        sorted.push(arr2[j])
+    }
+    return sorted
+}
+function mergeSort(array){
+    if(array.length > 1){
+        let mid = parseInt(array.length / 2)
+        let leftPart = mergeSort(array.slice(0, mid))
+        let rightPart = mergeSort(array.slice(mid, array.length))
+        let merged = merge(leftPart, rightPart)
+        return merged
+    }else{
+        return array
+    }
+}
+
+function removeDuplicates(array){
+    return array.filter((num, index)=> array.indexOf(num) === index)
+}
+
+function RDMergeSort(array){
+    let noDup = removeDuplicates(array)
+    return mergeSort(noDup)
+}
+
+let array = [10, 2, 7, 4, 4, 10, 7]
+console.log(RDMergeSort(array))
