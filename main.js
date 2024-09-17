ecb65d0d502e93b60aaba0bd6cf7c36233dc50ba
FadilMS18 <muhfadilms2418@gmail.com>
2024-09-17 14:53:12 +0800

    added find & levelOrder Func

--- main.js
+++ main.js
@@ -66,6 +66,51 @@ class BST {
     }
     return tree // return tree after every recursion
   }
+
+  static find(tree, value){
+    if(tree == null)return tree // return tree or null if value is not caught in the tree
+    if(tree.data == value){ // return tree if the given value is in the current tree
+      return tree
+    }
+    if(value > tree.data){ // run recursion to right subtree if value is higher than current data
+      return BST.find(tree.right, value)
+    }else{ // run recursion to the left subtree if the given value is less than the current data
+      return BST.find(tree.left, value)
+    }
+  }
+
+  static levelOrderLoops(tree){
+    // if(typeof(callback) !== 'function') throw new Error('Callback argument must be a function')
+    if(tree == null) return null
+    let array = []
+    let queue = []
+    queue.push(tree)
+    while(queue.length){
+      let current = queue.shift()
+      array.push(current.data)
+      if(current.left){
+        queue.push(current.left)
+      }
+      if(current.right){
+        queue.push(current.right)
+      }
+    }
+    return array
+  }
+
+  static levelOrderRecursion(tree ,array = [], queue = []){
+    if(tree == null)return
+    array.push(tree.data)
+
+    if(tree.left !== null) queue.push(tree.left)
+    if(tree.right !== null) queue.push(tree.right)
+
+    while(queue.length){
+      let current = queue.shift()
+      BST.levelOrderRecursion(current, array, queue)
+    }
+    return array   
+  }
 }
 
 
@@ -79,6 +124,13 @@ BST.insert(binarySearchTree, 50)
 // Delete Node Method
 BST.deleteItem(binarySearchTree, 67)
 
+// Function to visualize the tree
 prettyPrint(binarySearchTree)
 
-// prettyPrint(binarySearchTree);
+// Find function will return tree of given value, else will return null if given value is not in the tree  
+console.log(BST.find(binarySearchTree, 324))
+
+// Return an array with level order within the given tree
+console.log(BST.levelOrderLoops(binarySearchTree))
+console.log(BST.levelOrderRecursion(binarySearchTree))
+// Not sure what's callback should i use so i just use simple loops and recursion that will return an array of level order traversal
